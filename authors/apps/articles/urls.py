f4a59f590df98d62aec5d883e62fa85a0f89fd1e
myrdstom <nserekopaul@gmail.com>
2019-01-04 14:33:42 +0300

    Document the articles app[Delivers #162951717]

--- authors/apps/articles/urls.py
+++ authors/apps/articles/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import ArticleCreateListCreateView, ArticleDetailsView
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Articles API')
 
 urlpatterns = [
+    path('swagger-docs/', schema_view),
     path('articles/', ArticleCreateListCreateView.as_view(), name="create"),
     path('articles/<int:pk>/',
     ArticleDetailsView.as_view(), name="details"),
